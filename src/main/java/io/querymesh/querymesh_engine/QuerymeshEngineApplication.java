58e0b3a7a59869220121de875d84fab878d1e4fb
Luthy <ademolutip@gmail.com>
2026-04-21 07:30:07 +0100

    project packages initialization

---
+++ src/main/java/io/querymesh/querymesh_engine/QuerymeshEngineApplication.java
@@ -0,0 +1,13 @@
+package io.querymesh.querymesh_engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuerymeshEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuerymeshEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/querymesh/querymesh_engine/QuerymeshEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package io.querymesh.querymesh_engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuerymeshEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
